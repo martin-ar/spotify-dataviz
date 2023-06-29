4b6188d87f1f736cce70b20162b909804c6f72dc
Martin Aranda <65634847+martin-ar@users.noreply.github.com>
2023-06-29 22:02:43 +0000

    image fix dynamic scroll el

--- scripts.js
+++ scripts.js
@@ -15,118 +15,6 @@
 //   thumbnail.appendChild(clon);
 //   // thumbnail.innerHTML = clon;
 // }
-const scrollElements = document.querySelectorAll(".js-scroll");
-const throttleCount = document.getElementById("throttle-count");
-const scrollCount = document.getElementById("scroll-count");
-
-var throttleTimer;
-
-const throttle = (callback, time) => {
-  if (throttleTimer) return;
-
-  throttleTimer = true;
-  setTimeout(() => {
-    callback();
-    throttleTimer = false;
-  }, time);
-};
-
-const elementInView = (el, dividend = 1) => {
-  const elementTop = el.getBoundingClientRect().top;
-
-  return (
-    elementTop <=
-    (window.innerHeight || document.documentElement.clientHeight) / dividend
-  );
-};
-
-const elementOutofView = (el) => {
-  const elementTop = el.getBoundingClientRect().top;
-
-  return (
-    elementTop > (window.innerHeight || document.documentElement.clientHeight)
-  );
-};
-
-const displayScrollElement = (element) => {
-  element.classList.add("scrolled");
-  element.scrollTop = 0;
-};
-
-const hideScrollElement = (element) => {
-  element.classList.remove("scrolled");
-  element.scrollTop = 0;
-};
-
-const handleScrollAnimation = () => {
-  scrollElements.forEach((el) => {
-    if (elementInView(el, 1)) {
-      displayScrollElement(el);
-    } else if (elementOutofView(el)) {
-      hideScrollElement(el);
-    }
-  });
-};
-var timer = 0;
-var count = 0;
-var scroll = 0;
-
-window.addEventListener("scroll", () => {
-  scrollCount.innerHTML = scroll++;
-  throttle(() => {
-    handleScrollAnimation();
-    throttleCount.innerHTML = count++;
-  }, 500);
-});
-
-// bouton pour remonter la page
-let goUp = document.getElementById("go-up");
-
-// When the user scrolls down ()px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 2400 ||
-    document.documentElement.scrollTop > 2400
-  ) {
-    goUp.style.display = "block";
-  } else {
-    goUp.style.display = "none";
-  }
-}
-goUp.addEventListener("click", topFunction);
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTo({ top: 0, behavior: "smooth" }); // For Safari
-  document.documentElement.scrollTo({ top: 0, behavior: "smooth" }); // For Chrome, Firefox, IE and Opera
-}
-
-var line = document.getElementById("line");
-var length = line.getTotalLength();
-
-// The start position of the drawing
-line.style.strokeDasharray = length;
-
-// Hide the triangle by offsetting dash. Remove this line to show the triangle before scroll draw
-line.style.strokeDashoffset = length;
-
-// Find scroll percentage on scroll (using cross-browser properties), and offset dash same amount as percentage scrolled
-window.addEventListener("scroll", drawLine);
-
-function drawLine() {
-  var scrollpercent =
-    (document.body.scrollTop + document.documentElement.scrollTop) /
-    (document.documentElement.scrollHeight -
-      document.documentElement.clientHeight);
-
-  var draw = length * scrollpercent * 0.90;
-
-  // Reverse the drawing (when scrolling upwards)
-  line.style.strokeDashoffset = length - draw;
-}
 
 //JSON
 var json = null;
@@ -138,11 +26,60 @@ async function loadJsonAndFillCards(_id) {
       const response = await fetch("data.json"); // Fetch the JSON file
       json = await response.json(); // Parse the JSON response
 
+      createScrollElements(json.bars);
       fillCards(_id, json.bars); // Call the 'fillCards' function with the JSON data
     } catch (error) {
       console.error(error);
     }
   }
+}
+
+function createScrollElements(jsonBars) {
+  const container = document.querySelector('.scroll-section');
+  
+  jsonBars.forEach((bar, index) => {
+    // Create the main container element
+    const elementContainer = document.createElement('article');
+    elementContainer.classList.add('element-container');
+    
+    // Create the thumbnail element
+    const thumbnail = document.createElement('div');
+    const slideDirection = index % 2 === 0 ? 'slide-left' : 'slide-right';
+    thumbnail.classList.add('scroll-element', 'js-scroll', slideDirection, 'thumbnail');
+    thumbnail.id = `bar_${bar.id}`;
+    elementContainer.appendChild(thumbnail);
+    
+    // Create the full-size element
+    const fullSize = document.createElement('div');
+    fullSize.classList.add('box', 'full-size');
+    fullSize.id = `full-bar-${bar.id}`;
+    elementContainer.appendChild(fullSize);
+    
+    // Create the close button wrapper
+    const closeWrapper = document.createElement('div');
+    closeWrapper.classList.add('close-full-size-wrapper');
+    fullSize.appendChild(closeWrapper);
+    
+    // Create the close button
+    const closeButton = document.createElement('span');
+    closeButton.classList.add('glyphicon', 'glyphicon-remove-circle', 'close-full-size');
+    closeButton.setAttribute('aria-label', 'Close');
+    closeWrapper.appendChild(closeButton);
+    
+    // Create the full-size image
+    const fullSizeImg = document.createElement('img');
+    fullSizeImg.classList.add('full-size-img');
+    fullSizeImg.src = `assets/img/${bar.fullCard.path}`;
+    fullSize.appendChild(fullSizeImg);
+    
+    // Create the placeholder element
+    const placeholder = document.createElement('div');
+    placeholder.classList.add('placeholder');
+    elementContainer.appendChild(placeholder);
+    
+    // Append the element container to the main container
+    container.appendChild(elementContainer);
+  });
 }
 //Create cards
 function fillCards(templateId, jsonBars) {
@@ -156,23 +93,23 @@ function fillCards(templateId, jsonBars) {
     const card = previewCard.querySelector('.card');
     card.style.backgroundColor = `#${bar.hexColor.primary[1]}`;
     card.style.border = `2px solid #${bar.hexColor.primary[1]}`;
-
+    
     // Get the card content elements
     const title = previewCard.querySelector(".title");
-
+    
     if (title) {
       title.textContent = bar.name;
       title.style.color = `#${bar.hexColor.primary[0]}`;
     }
-
+    
     const tags = previewCard.querySelector(".tags-wrapper");
     if (tags) {
       tags.innerHTML = bar.tags
-        .map((tag) => `<span class="tag" style="color:#${bar.hexColor.primary[0]} !important;border-color:#${bar.hexColor.primary[0]} !important;" >${tag}</span>`)
-        .join("");
+      .map((tag) => `<span class="tag" style="color:#${bar.hexColor.primary[0]} !important;border-color:#${bar.hexColor.primary[0]} !important;" >${tag}</span>`)
+      .join("");
       const knowMoreButton = document.createElement("div");
       knowMoreButton.innerHTML =
-        `<span class="button is-primary nobr open-full-size" id="open-bar-${bar.id}" style="background-color:#${bar.hexColor.primary[0]} !important;color:#${bar.hexColor.primary[1]} !important;">En savoir plus sur les playlists...</span>`;
+      `<span class="button is-primary nobr open-full-size" id="open-bar-${bar.id}" style="background-color:#${bar.hexColor.primary[0]} !important;color:#${bar.hexColor.primary[1]} !important;">En savoir plus sur les playlists...</span>`;
       // knowMoreButton.classList.add("open-full-size");
       tags.appendChild(knowMoreButton);
     }
@@ -183,26 +120,26 @@ function fillCards(templateId, jsonBars) {
     }
     const aside = previewCard.querySelector('.info');
     const asideColor = bar.name === 'Atomic Cat' ? `#${bar.hexColor.alternative[1]}` : `#${bar.hexColor.primary[0]}`;
-
+    
     aside.style.backgroundColor = asideColor;
     aside.style.color = '#FFFFFF';
-
+    
     const address = previewCard.querySelector(".container:nth-child(1)");
     if (address) {
       address.innerHTML = `<p><span class='nobr'>${bar.address.addressLine1}</span><br>${bar.address.zip} ${bar.address.city}</p>`;
     }
-
+    
     const openHours = previewCard.querySelector(".container:nth-child(2)");
     if (openHours) {
       openHours.innerHTML = `<p><span class='nobr'>${bar.openDay}</span><br><span class='nobr'>${bar.openTime}</span></p>`;
     }
-
+    
     const transports = previewCard.querySelector(".container:nth-child(3)");
     if (transports) {
       const metroLines = `<p>${bar.transports.join(' ')}</p>`;
       transports.innerHTML = `<div class="directions">${metroLines}<p><span class="nobr">${bar.metroStationName}</p></span></div>`;
     }
-
+    
     // Add the preview card to the document
     const preview = document.getElementById("bar_" + bar.id.toString());
     if (preview) {
@@ -212,53 +149,169 @@ function fillCards(templateId, jsonBars) {
   const elementContainers = document.querySelectorAll('.element-container');
   const lastElementContainer = elementContainers[elementContainers.length - 1];
   lastElementContainer.style.marginBottom = '280px';
-
+  
 }
 
 //greensock
 gsap.registerPlugin(Flip, Draggable);
 
-const scrollSection = document.querySelector(".scroll-section");
+window.onload = function() {
 
-scrollSection.addEventListener("click", (event) => {
-  var clickedElement = event.target;
+  const scrollSection = document.querySelector(".scroll-section");
 
-  if (clickedElement.classList.contains("open-full-size")) {
-    const container = clickedElement.closest(".element-container");
-    const fullSize = container.querySelector(".full-size");
-    const thumbnail = container.querySelector(".thumbnail");
-    const closeButton = container.querySelector(".close-full-size");
+  scrollSection.addEventListener("click", (event) => {
+    var clickedElement = event.target;
+    
+    if (clickedElement.classList.contains("open-full-size")) {
+      const container = clickedElement.closest(".element-container");
+      const fullSize = container.querySelector(".full-size");
+      const thumbnail = container.querySelector(".thumbnail");
+      const closeButton = container.querySelector(".close-full-size");
+      
+      thumbnail.classList.toggle("active");
+      fullSize.classList.toggle("active");
+      
+      Draggable.create(fullSize);
+      Draggable.create(clickedElement);
+      
+      clickedElement.fullSize = fullSize;
+      clickedElement.thumbnail = thumbnail;
+      clickedElement.close = closeButton;
+      
+      clickedElement.scrollTop = 0;
+      window.addEventListener("mousedown", closeFullSizeHandler);
+      closeButton.addEventListener("click", closeFullSizeHandler);
+    }
+    function closeFullSizeHandler() {
+      const fullSize = clickedElement.fullSize;
+      const thumbnail = clickedElement.thumbnail;
+      const closeButton = clickedElement.close;
+      
+      closeFullSize(fullSize, thumbnail);
+      window.removeEventListener("mousedown", closeFullSizeHandler);
+      closeButton.removeEventListener("click", closeFullSizeHandler);
+    }
+  });
 
-    thumbnail.classList.toggle("active");
+
+  function closeFullSize(fullSize, thumbnail) {
+    
+    fullSize.scrollTop = 0;
+    thumbnail.scrollTop = 0;
     fullSize.classList.toggle("active");
-
-    Draggable.create(fullSize);
-    Draggable.create(clickedElement);
-
-    clickedElement.fullSize = fullSize;
-    clickedElement.thumbnail = thumbnail;
-    clickedElement.close = closeButton;
-
-    clickedElement.scrollTop = 0;
-    window.addEventListener("mousedown", closeFullSizeHandler);
-    closeButton.addEventListener("click", closeFullSizeHandler);
+    thumbnail.classList.toggle("active");
   }
-  function closeFullSizeHandler() {
-    const fullSize = clickedElement.fullSize;
-    const thumbnail = clickedElement.thumbnail;
-    const closeButton = clickedElement.close;
-  
-    closeFullSize(fullSize, thumbnail);
-    window.removeEventListener("mousedown", closeFullSizeHandler);
-    closeButton.removeEventListener("click", closeFullSizeHandler);
+
+  const scrollElements = document.querySelectorAll(".js-scroll");
+  const throttleCount = document.getElementById("throttle-count");
+  const scrollCount = document.getElementById("scroll-count");
+
+  var throttleTimer;
+
+  const throttle = (callback, time) => {
+    if (throttleTimer) return;
+
+    throttleTimer = true;
+    setTimeout(() => {
+      callback();
+      throttleTimer = false;
+    }, time);
+  };
+
+  const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+      elementTop <=
+      (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+  };
+
+  const elementOutofView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+      elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
+  };
+
+  const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+    element.scrollTop = 0;
+  };
+
+  const hideScrollElement = (element) => {
+    element.classList.remove("scrolled");
+    element.scrollTop = 0;
+  };
+
+  const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+      if (elementInView(el, 1)) {
+        displayScrollElement(el);
+      } else if (elementOutofView(el)) {
+        hideScrollElement(el);
+      }
+    });
+  };
+  var timer = 0;
+  var count = 0;
+  var scroll = 0;
+
+  window.addEventListener("scroll", () => {
+    scrollCount.innerHTML = scroll++;
+    throttle(() => {
+      handleScrollAnimation();
+      throttleCount.innerHTML = count++;
+    }, 500);
+  });
+
+  // bouton pour remonter la page
+  let goUp = document.getElementById("go-up");
+
+  // When the user scrolls down ()px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 2400 ||
+      document.documentElement.scrollTop > 2400
+    ) {
+      goUp.style.display = "block";
+    } else {
+      goUp.style.display = "none";
+    }
   }
-});
+  goUp.addEventListener("click", topFunction);
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTo({ top: 0, behavior: "smooth" }); // For Safari
+    document.documentElement.scrollTo({ top: 0, behavior: "smooth" }); // For Chrome, Firefox, IE and Opera
+  }
 
+  var line = document.getElementById("line");
+  var length = line.getTotalLength();
 
-function closeFullSize(fullSize, thumbnail) {
+  // The start position of the drawing
+  line.style.strokeDasharray = length;
 
-  fullSize.scrollTop = 0;
-  thumbnail.scrollTop = 0;
-  fullSize.classList.toggle("active");
-  thumbnail.classList.toggle("active");
+  // Hide the triangle by offsetting dash. Remove this line to show the triangle before scroll draw
+  line.style.strokeDashoffset = length;
+
+  // Find scroll percentage on scroll (using cross-browser properties), and offset dash same amount as percentage scrolled
+  window.addEventListener("scroll", drawLine);
+
+  function drawLine() {
+    var scrollpercent =
+      (document.body.scrollTop + document.documentElement.scrollTop) /
+      (document.documentElement.scrollHeight -
+        document.documentElement.clientHeight);
+
+    var draw = length * scrollpercent * 0.90;
+
+    // Reverse the drawing (when scrolling upwards)
+    line.style.strokeDashoffset = length - draw;
+  }
 }
